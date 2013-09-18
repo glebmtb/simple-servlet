314d847bc5d229866e321af8a6e40b5e477f67cb
Belyaev Gleb <Belyaev.Gleb@cg.ru>
2013-09-18 08:44:55 +0400

    передача параметров в GET

---
+++ src/main/java/ParamServlet.java
@@ -0,0 +1,38 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class ParamServlet extends HttpServlet
+{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        String name = req.getParameter("name");
+
+        System.out.println(name);
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+
+        //получаем ссылку на объект PrintWriter, которая даёт возможность сервлету отправлять содержимое клиенту.
+        try (PrintWriter out = resp.getWriter())
+        {
+            //переедаем HTML страницу
+            out.print("<!DOCTYPE html>\n" +
+                    "<html>\n" +
+                    "<body>\n" +
+                    "\n" +
+                    "<h1>Мой первый Servlet</h1>\n" +
+                    "\n" +
+                    "<p>" + name + ", добро пожаловать  в Servlet</p>\n" +
+                    "\n" +
+                    "</body>\n" +
+                    "</html>");
+        }
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,6 +4,7 @@
     <title></title>
 </head>
 <body>
-      <a href="servlets/WelcomeServlet.html">1. Простой пример сервлета</a>
+<a href="servlets/WelcomeServlet.html">1. Простой пример сервлета</a> <br>
+<a href="servlets/WelcomeServlet2.html">2. Пример с передачей параметра</a>
 </body>
 </html>
